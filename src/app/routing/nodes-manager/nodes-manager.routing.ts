f892a4f25deb725af26dfcaf90425d0a89f11b9d
Farid <f.niyazli@bitstudios.com>
2024-09-10 19:25:27 +0400

    node tree + drag and drop

---
+++ src/app/routing/nodes-manager/nodes-manager.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { NodesManagerComponent } from './pages/nodes-manager/nodes-manager.component';
+
+export const nodesManagerRouting: Routes = [
+  {
+    path: '',
+    component: NodesManagerComponent,
+  },
+];
