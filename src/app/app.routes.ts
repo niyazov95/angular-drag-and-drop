e867634337fca113efe491349ae6ab9fdb007313
Farid <f.niyazli@bitstudios.com>
2024-09-11 12:35:05 +0400

    add default router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'node-manager',
+    pathMatch: 'full',
+  },
+  {
     path: 'node-manager',
     loadChildren: () =>
       import('./routing/nodes-manager/nodes-manager.routing').then(
